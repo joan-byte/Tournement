5d651d6f54f6e67cdbc55847793cddb4049aa2da
Joan Alba <picasoca@me.com>
2023-10-04 11:57:57 +0200

    paras resultado aranking en creacion correcto

--- ranking/utils.py
+++ ranking/utils.py
@@ -4,14 +4,19 @@ from inscripcion.models import Parejas
 
 def actualizar_ranking(resultado):
     from .models import Ranking
-    """
-    Esta función toma un objeto Resultado y actualiza el ranking de las parejas involucradas.
-    """
+
+    # Verificando si los números de pareja son válidos antes de continuar
+    if not resultado.n_pareja_uno or not resultado.n_pareja_dos:
+        print("Error: Los números de pareja no son válidos.")
+        return
+
     print("Iniciando la actualización del ranking...")
 
     # Actualizar el ranking de la pareja_uno si es válido
     if resultado.n_pareja_uno and resultado.n_pareja_uno != 0:
-        pareja_uno_instance, _ = Parejas.objects.get_or_create(Numero_pareja=resultado.n_pareja_uno)
+        pareja_uno_instance, created = Parejas.objects.get_or_create(id=resultado.n_pareja_uno)
+        if created:
+            print(f"Creada nueva instancia de Parejas para pareja_uno con número {resultado.n_pareja_uno}")
         ranking_pareja_uno, _ = Ranking.objects.get_or_create(Numero_pareja=pareja_uno_instance)
         ranking_pareja_uno.Nombre_pareja = resultado.nombre_pareja_uno
         ranking_pareja_uno.puntos = (ranking_pareja_uno.puntos if ranking_pareja_uno.puntos else 0) + resultado.puntos_pareja_uno
@@ -25,14 +30,16 @@ def actualizar_ranking(resultado):
 
     # Actualizar el ranking de la pareja_dos si es válido
     if resultado.n_pareja_dos and resultado.n_pareja_dos != 0:
-        pareja_dos_instance = Parejas.objects.get(id=resultado.n_pareja_dos)
+        pareja_dos_instance, created = Parejas.objects.get_or_create(id=resultado.n_pareja_dos)
+        if created:
+            print(f"Creada nueva instancia de Parejas para pareja_dos con número {resultado.n_pareja_dos}")
         ranking_pareja_dos, _ = Ranking.objects.get_or_create(Numero_pareja=pareja_dos_instance)
         ranking_pareja_dos.Nombre_pareja = resultado.nombre_pareja_dos
         ranking_pareja_dos.puntos = (ranking_pareja_dos.puntos if ranking_pareja_dos.puntos else 0) + resultado.puntos_pareja_dos
         ranking_pareja_dos.partida = (ranking_pareja_dos.partida if ranking_pareja_dos.partida else 0) + 1
         if resultado.puntos_pareja_dos > 0:
             ranking_pareja_dos.partidas_ganadas = 1
-        else:    
+        else:
             ranking_pareja_dos.partidas_ganadas = 0
         ranking_pareja_dos.save()
         print(f"Pareja dos ({resultado.n_pareja_dos}): {resultado.puntos_pareja_dos} puntos.")
@@ -44,4 +51,3 @@ def actualizar_ranking(resultado):
         ranking.rank = rank_position
         ranking.save()
         rank_position += 1
-    
